aac9ee70b11cff8c0abe195dc3a71d5eb830cab2
sppsocialmarketing <sppsocialmarketing@gmail.com>
2026-04-10 12:34:28 -0700

    Fixed Fireflies

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   const fireflies = document.querySelectorAll(".firefly");
+  let mouseX = 0;
+  let mouseY = 0;
+  let currentX = 0;
+  let currentY = 0;
 
   window.addEventListener("mousemove", (event) => {
-    const x = (event.clientX / window.innerWidth - 0.5) * 10;
-    const y = (event.clientY / window.innerHeight - 0.5) * 10;
+    mouseX = (event.clientX / window.innerWidth - 0.5) * 10;
+    mouseY = (event.clientY / window.innerHeight - 0.5) * 10;
+  }, { passive: true });
+
+  function tick() {
+    currentX += (mouseX - currentX) * 0.06;
+    currentY += (mouseY - currentY) * 0.06;
 
     fireflies.forEach((fly, index) => {
       const factor = (index % 4 + 1) * 0.35;
-      fly.style.marginLeft = `${x * factor}px`;
-      fly.style.marginTop = `${y * factor}px`;
+      fly.style.transform = `translate3d(${currentX * factor}px, ${currentY * factor}px, 0)`;
     });
-  }, { passive: true });
+
+    requestAnimationFrame(tick);
+  }
+
+  tick();
 });
